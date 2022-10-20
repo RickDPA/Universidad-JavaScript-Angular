f8d78a28f5d094890f1eda8642ade50e4ed9d7be
RickDPA <ricardodiegopenagosarenas83@gmail.com>
2022-10-20 00:07:39 -0500

    segundo commit

---
+++ presupuesto-app/src/app/ingreso/ingreso.servicio.ts
@@ -0,0 +1,11 @@
+import { Ingreso } from "./ingreso.model";
+
+export class IngresoServicio{
+    
+    ingresos:Ingreso[] = [
+
+        new Ingreso("Salario", 4000),
+        new Ingreso("Venta Coche", 500)
+    ];
+
+}
