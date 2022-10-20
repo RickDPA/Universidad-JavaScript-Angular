28e9a0979fee7b87a70b8e51c6b02e26b1124639
RickDPA <ricardodiegopenagosarenas83@gmail.com>
2022-10-20 00:13:14 -0500

    segundo commit v2

--- listado-personas/src/app/formulario/formulario.component.ts
+++ listado-personas/src/app/formulario/formulario.component.ts
@@ -1,20 +1,47 @@
-import { Component, EventEmitter, Output } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import { LoggingService } from '../LoggingService.service';
 import { Persona } from '../persona.model';
+import { PersonasService } from '../personas.service';
 
 @Component({
   selector: 'app-formulario',
   templateUrl: './formulario.component.html',
   styleUrls: ['./formulario.component.css']
 })
-export class FormularioComponent {
+export class FormularioComponent implements OnInit{
   
-  @Output() personaCreada = new EventEmitter<Persona>();
+  //@Output() personaCreada = new EventEmitter<Persona>();
 
   nombreInput:string = "";
   apellidoInput:string = "";
+  //@ViewChild("nombreInput") nombreInput:ElementRef ;
+  //@ViewChild("apellidoInput") apellidoInput:ElementRef;
+
+  constructor(private loggingService:LoggingService,
+              private personasService:PersonasService){
+
+    this.personasService.saludar.subscribe(
+      (indice:number) => alert("El indice es: " + (indice + 1))
+    );
+  
+    
+  }
+
+  ngOnInit(): void {
+      
+  }
+
 
   agregarPersona(){
-    let persona1 = new Persona(this.nombreInput, this.apellidoInput);
-    this.personaCreada.emit(persona1);
+    let persona1 = new Persona(
+
+      this.nombreInput,  //.nativeElement.value 
+      this.apellidoInput //.nativeElement.value
+    );
+
+    //this.loggingService.enviarMensajeAConsola("Enviamos persona:" + persona1.nombre + " apellido: " + persona1.apellido);
+    //this.personaCreada.emit(persona1);
+
+    this.personasService.agregarPersona(persona1);
   }
 }
